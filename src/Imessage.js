ef33effdc8ee41894e53880e229aa685f780ebd1
alexanderajju <alexanderajju@gmail.com>
2020-10-25 14:33:19 +0530

    side bar added

--- src/Imessage.js
+++ src/Imessage.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Imessage.css'
-
+import Sidebar from './Sidebar'
 function Imessage() {
     return (
         <div className="imessage">
             <h1>Imessage</h1>
-            
+            <Sidebar/>
         </div>
     )
 }
