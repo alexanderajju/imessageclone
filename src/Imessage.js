5ce68d720e7376798a4715fa294d1f87a838c077
alexanderajju <alexanderajju@gmail.com>
2020-10-25 14:28:55 +0530

    imessage

---
+++ src/Imessage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Imessage.css'
+
+function Imessage() {
+    return (
+        <div className="imessage">
+            <h1>Imessage</h1>
+            
+        </div>
+    )
+}
+
+export default Imessage
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import counterReducer from '../features/counterSlice';
+import userReducer from '../user/userSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    user:userReducer,
   },
 });
